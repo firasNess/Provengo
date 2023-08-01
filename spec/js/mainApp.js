2efa5dd43324805dde952a4c627d6ed07c839691
firasNess <p0035254@ness-tech.co.il>
2023-08-01 13:32:40 +0300

    add break piont

--- spec/js/mainApp.js
+++ spec/js/mainApp.js
@@ -5,9 +5,9 @@ bthread("main", function(){
     request(Actions.openApp());
     checkConnection();
     
-        let operationIcon = choose("חינה", "פיקוח");
+        let operationIcon = choose("חניה", "פיקוח");
 
-        if (operationIcon === "חינה"){
+        if (operationIcon === "חניה"){
             checkConnection();
             request(Actions.clickButton("חינה"));
             let option = choose("דוח חניה חדש", "דוחות אחרונים")
@@ -53,7 +53,7 @@ bthread("main", function(){
                     
                     request(Actions.fromImg("הוספת תמונת דוח על שמשת הרכב"));
                     request(Actions.uploadImg("תמונת הרכב"));
-                    Ctrl.doMark("Full Scenario");
+                    Ctrl.doMark("End of Scenario");
 
 
                     } else {
@@ -70,10 +70,11 @@ function processOfTicket (){
 
     if (maybe("למלא את ביקורת")){
         checkConnection();
+        // to do : function to run like before function
         request(Actions.writeInTextField("ביקורת"));
         request(Actions.writeText("23"));
     } else {
-        request(Actions.fail("חובה למלא את שדה ביקורת"));
+        bp.ASSERT(false, " חובה למלא את שדה ביקורת");
     }
 
 
@@ -82,7 +83,7 @@ function processOfTicket (){
         request(Actions.writeInTextField("מספר דוח"));
         request(Actions.writeText("123"));
     } else {
-        request(Actions.fail(" חובה למלא את שדה מספר דוח"));
+        bp.ASSERT(false, "חובה למלא את שדה מספר דוח");
     }
 
     if (maybe("למלא את סעיף")){
@@ -90,7 +91,7 @@ function processOfTicket (){
         request(Actions.chooseFromDropDown("סעיף"));
         request(Actions.pickOption("1"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה סעיף");
     }
 
     if (maybe("למלא את הערה מובנית")){
@@ -98,8 +99,8 @@ function processOfTicket (){
         request(Actions.chooseFromDropDown("הערה מובנית"));
         request(Actions.pickOption("אין"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
-    }
+        bp.ASSERT(false, "חובה למלא את שדה הערה מובנית");
+        }
 
 
     if (maybe("למלא את שיטת מסירה")){
@@ -107,7 +108,7 @@ function processOfTicket (){
         request(Actions.chooseFromDropDown("שיטת מסירה"));
         request(Actions.pickOption("רגיל"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה שיטת מסירה");
     }
 
     if (maybe("למלא את רחוב")){
@@ -115,7 +116,7 @@ function processOfTicket (){
         request(Actions.writeInTextField("רחוב"));
         request(Actions.writeText("דניאל"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה רחוב");
     }
 
     if (maybe("למלא את מספר בית")){
@@ -123,7 +124,7 @@ function processOfTicket (){
         request(Actions.writeInTextField("מספר בית"));
         request(Actions.writeText("1"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה מספר בית");
     }
 
     if (maybe("למלא את שכונה ")){
@@ -131,7 +132,7 @@ function processOfTicket (){
         request(Actions.writeInTextField("שכונה"));
         request(Actions.writeText("מורשה"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה שכונה ");
     }
 
     if (maybe("למלא את ציבורי ")){
@@ -139,7 +140,7 @@ function processOfTicket (){
         request(Actions.fromRadioButoon("ציבורי"));
         request(Actions.clickOnRadioButton("להדליק"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה ציבורי");
     }
 
     if (maybe("למלא את מול/ליד ")){
@@ -147,7 +148,7 @@ function processOfTicket (){
         request(Actions.chooseFromDropDown("ליד/מול"));
         request(Actions.pickOption("ליד"));
     } else {
-        request(Actions.fail("חובה למלא את השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה מול/ליד ");
     }
 
     if (maybe(" למלא את תאור המקום ")){
@@ -155,31 +156,31 @@ function processOfTicket (){
         request(Actions.writeInTextField("תאור המקום"));
         request(Actions.writeText("כלום"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה תאור המקום");
     }
 
-    if (maybe(" למלא את תמונת תמרור ")){
+    if (maybe("למלא את תמונת תמרור ")){
         checkConnection();
         request(Actions.fromImg("תמונת תמרור"));
         request(Actions.writeText("תמונה אישית"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה תמונת תמרור");
     }
 
-    if (maybe(" למלא את תמונת חניה ")){
+    if (maybe("למלא את תמונת חניה ")){
         checkConnection();
         request(Actions.fromImg("תמונת חניה"));
         request(Actions.writeText("תמונה לרכב"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה תמונת חניה");
     }
 
-    if (maybe(" למלא את הערות ")){
+    if (maybe("למלא את הערות ")){
         checkConnection();
         request(Actions.writeInTextField("הערות"));
         request(Actions.writeText("אין"));
     } else {
-        request(Actions.fail("חובה למלא את שדה השדה"));
+        bp.ASSERT(false, "חובה למלא את שדה הערות");
     }
 
 }
